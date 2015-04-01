33f8983460d7308278b7d4d597623dcae50b2f4f
Brian O'Neil <brian.oneil@turner.com>
2015-03-31 23:32:45 -0400

    updated version, readme, and cleaned up route check

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+function isRoute(route) {
+  var hasHandler = route.handler || (route.config && route.config.handler);
+  var hasPath = route.path;
+  return hasHandler && hasPath;
+}
+
+
 module.exports = function(mod) {
   var debug = require('debug')('require-hapiroutes:routediscovery')
   var requireDirectory = require('require-directory');
   var dirModules = requireDirectory(mod);
-
   var routes = [];
-
   var keys = Object.keys( dirModules );
+
+
   for( var i = 0,length = keys.length; i < length; i++ ) {
     var routeModule = dirModules[ keys[ i ] ];
 
@@ -15,14 +22,12 @@ module.exports = function(mod) {
       Array.prototype.push.apply(routes, routeModule.routes);
     }
 
-    debug('isArray: %s', Object.prototype.toString.call(routeModule) === '[object Array]')
-
-    if(Array.isArray(routeModule) && routeModule.length > 0 && routeModule[0].hasOwnProperty('handler') && routeModule[0].hasOwnProperty('path')) {
-      debug('this looks like an array for some reason')
+    if(Array.isArray(routeModule) && routeModule.length > 0 && isRoute(routeModule[0])) {
+      debug('loading routes from module as an Array from file: %s', keys[i])
       Array.prototype.push.apply(routes, routeModule);
     }
 
-    else if( (routeModule.hasOwnProperty('handler') || (routeModule.hasOwnProperty('config') && routeModule.config.hasOwnProperty('handler')) ) && routeModule.hasOwnProperty('path') && (routeModule.handler || routeModule.config.handler) && routeModule.path){
+    else if(isRoute(routeModule)){
       debug('loading module as a route file: %s', keys[i]);
       routes.push(routeModule);
     }
